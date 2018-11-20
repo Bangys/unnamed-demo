e45dc86e43f147639e121fed1f4d485272432c80
yongsan@vip.qq.com <yongsan@vip.qq.com>
2018-11-20 23:37:32 +0800

    add jinja2 and bootstrap

--- demo.py
+++ demo.py
@@ -1,16 +1,27 @@
 # -*- coding:utf-8 -*-
 
 
-from flask import Flask, make_response, request
+from flask import Flask, make_response, request, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-
+bootstrap = Bootstrap(app)
 @app.route('/')
 def index():
     user_agent = request.headers.get('User-Agent')
-    response = make_response("<h1>what's on your mind?</h1>", 666)
-    return response
+    return render_template('index.html',ua=user_agent)
 
+@app.route('/user/<name>') 
+def user(name):
+    return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('404.html'), 500
 
 if '__main__' == __name__:
 	app.run()
