df8dc9548d79c3d22094019ae920d4074e5899c8
bangysn <bangysn@gmail.com>
2019-05-26 23:23:10 +0800

    udpate navbar

---
+++ app/static/js/custom.js
@@ -0,0 +1,43 @@
+var initedEditor = function() {
+    var e = new Editor()
+    var element = $('.editor').get(0)
+    e.render(element)
+    return e
+}
+
+
+var trigger = $('.hamburger'),
+    overlay = $('.overlay'),
+    isClosed = false;
+
+trigger.click(function() {
+    hamburger_cross();
+});
+
+function hamburger_cross() {
+
+    if (isClosed == true) {
+        overlay.hide();
+        // trigger.removeClass('is-open');
+        // trigger.addClass('is-closed');
+        isClosed = false;
+    } else {
+        overlay.show();
+        // trigger.removeClass('is-closed');
+        // trigger.addClass('is-open');
+        isClosed = true;
+    }
+}
+
+$('[data-toggle="offcanvas"]').click(function() {
+    $('#wrapper').toggleClass('toggled');
+});
+
+
+var __main = function() {
+    initedEditor()
+}
+
+$(document).ready(function() {
+    __main()
+})
